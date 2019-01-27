6c4e870a2b49532d41e113c19ebc7b7e7ce3becd
romzik <denrom@meta.ua>
2019-01-28 00:17:29 +0200

    try

--- src/com/amadeus/birds/Main.java
+++ src/com/amadeus/birds/Main.java
@@ -5,7 +5,38 @@ public class Main
 
     public static void main(String[] args)
     {
+        Shop shop = new Shop();
+        shop.addBird("crow", 1);
+        shop.addBird("eagle", 5);
+        shop.addBird("duck", 0.5);
+
+        shop.addBirdStorage("crow", 20);
+        shop.addBirdStorage("eagle", 23);
+        shop.addBirdStorage("duck", 4);
+
+        shop.buyBirds("Customer1" , "crow");
+        shop.buyBirds("Customer1" , "crow");
+        shop.buyBirds("Customer1" , "eagle");
+        shop.buyBirds("Customer1" , "duck");
+
+        shop.buyBirds("Customer2" , "eagle");
+        shop.buyBirds("Customer2" , "eagle");
+        shop.buyBirds("Customer2" , "eagle");
+        shop.buyBirds("Customer2" , "duck");
+
+        shop.buyBirds("Customer2" , "duck");
+        shop.buyBirds("Customer2" , "duck");
+        shop.buyBirds("Customer2" , "duck");
+
         System.out.println("Demo asdasdasd!!!");
+
+        System.out.println("Birds sale "+ shop.getBirdsSale("crow") + "ворон на сумму" + shop.getSoldBirdsMoney("crow"));
+        System.out.println("Birds sale "+ shop.getBirdsSale("eagle") + "орлов на сумму" + shop.getSoldBirdsMoney("eagle"));
+        System.out.println("Birds sale "+ shop.getBirdsSale("duck") + "уток на сумму" + shop.getSoldBirdsMoney("duck"));
+        System.out.println("Нас кладе меньше 3-ёх птиц" + shop.getBirdsLessThanThree());
+        System.out.println("Заработано денег" + shop.getTotalMoney());
+        System.out.println("Список покупателей отсортирован по количеству потраченных денег" + shop.sortCumtomerByMoney());
+        System.out.println("Cписок покупателей отсортирован по количеству совершённых покупок" + shop.sortCustomerByQuantityBirds());
 
     }
 }
--- src/com/amadeus/birds/Shop.java
+++ src/com/amadeus/birds/Shop.java
@@ -1,2 +1,38 @@
+//package com.amadeus.birds;
+
 public class Shop {
+    private String [] price;
+    private String [] quantity;
+    private String [] transactions;
+
+    public void addBird(String birdName, int price){
+
+    }
+
+    public void addBirdStorage(String birdName, int quantity){
+
+    }
+
+    public void buyBirds(String customer, String nameBird){
+
+    }
+
+    public int getBirdsSale(String nameBird){
+        return ;
+    }
+    public int getSoldBirdsMoney(String nameBird){
+        return ;
+    }
+    public String getBirdsLessThanThree(){
+        return ;
+    }
+    public int getTotalMoney(){
+        return;
+    }
+    public String sortCumtomerByMoney(){
+
+    }
+    public String sortCustomerByQuantityBirds(){
+        return;
+    }
 }
